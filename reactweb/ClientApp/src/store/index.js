6c4cadc701146094595af024aa49f4efe330afd6
Sairama Jamalapuram <sairamaj@hotmail.com>
2018-10-17 21:01:04 +0000

    removed test project

--- reactweb/ClientApp/src/store/index.js
+++
@@ -1,5 +0,0 @@
-import { createStore } from "redux";
-import reducer from "../reducers";
-
-const initialState = { tips: [] };
-export const store = createStore(reducer, initialState);                                                                                                                                                                                                                        
